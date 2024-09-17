b23a94b2a56af1e1739db3da9510665026f21302
Josué Silva de Morais <josuemorais@gmail.com>
2024-09-17 10:40:01 -0300

    refactor esp32

--- src/esp1.cpp
+++ src/esp1.cpp
@@ -8,7 +8,7 @@
 
 void plotWave(void *)
 {
-  uint16_t timeWave = 0;                          // Índice para percorrer a tabela de formas de onda
+  uint16_t timeWave = 0;
   AsyncDelay_c delayPlotWave(TIME_DELAY, ISMICROS); // time in micro second
   for (;;)
   {
@@ -16,24 +16,10 @@ void plotWave(void *)
     {
       delayPlotWave.repeat();
       const double aux = 1.0 + sin(2.0 * PI * CILCE_FREQ * timeWave);
-      ledcWrite(0, uint16_t(511.0*aux));
-      dacWrite(def_pin_DAC1, uint8_t(127.0*aux));
-      if (++timeWave >= CILCE_PERIODO) timeWave = 0;
-    }
-  }
-}
-
-static uint32_t time_delay_analog_ms = 1;
-void readWave(void *)
-{
-  uint32_t previousTimeAnalogMS = 0;  
-  for (;;)
-  {
-    uint32_t currentMilis = millis();
-    if ((currentMilis - previousTimeAnalogMS) >= time_delay_analog_ms)
-    {
-      previousTimeAnalogMS = currentMilis;
-      analogRead(def_pin_DAC1);
+      ledcWrite(0, uint16_t(511.0 * aux));
+      dacWrite(def_pin_DAC1, uint8_t(127.0 * aux));
+      if (++timeWave >= CILCE_PERIODO)
+        timeWave = 0;
     }
   }
 }
@@ -44,11 +30,9 @@ void setup()
   ledcAttachPin(def_pin_PWM, 0);
   ledcSetup(0, 1000, 10);
   xTaskCreate(plotWave, "Task Wave", 5000, NULL, 1, NULL);
-  xTaskCreate(readWave, "Task Wave", 5000, NULL, 1, NULL);  
 }
 
 void loop()
 {
   IIKit.loop();
-  time_delay_analog_ms = map(analogRead(def_pin_POT1), 0, 4095, 0, 120);
 }
