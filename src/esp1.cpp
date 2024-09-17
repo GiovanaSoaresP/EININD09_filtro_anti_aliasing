3a2714da25ed7063c500e4275e1d30b8454c0084
josuemoraisgh <josuemorais@gmail.com>
2024-09-17 13:58:56 -0300

    refactor ota.h

--- src/esp1.cpp
+++ src/esp1.cpp
@@ -1,39 +1,28 @@
+// https://forum.arduino.cc/t/how-to-make-tasks-and-determine-stack-size-in-freertos/978325/13
 #include <Arduino.h>
 #include "IiKit.h"
 
-#define FREQ 50.0
-#define TIME_DELAY 100.0 // Time em microsegundos
-#define CILCE_FREQ (FREQ * TIME_DELAY / 1000000.0)
+#define FREQ 5
+#define TIME_DELAY 1.0 // Time em milisegundos
+#define CILCE_FREQ (FREQ * TIME_DELAY / 1000.0)
 #define CILCE_PERIODO (1.0 / CILCE_FREQ)
+
+static uint32_t time_delay_analog_ms = 1;
 
 void plotWave(void *)
 {
   uint16_t timeWave = 0;                          // Índice para percorrer a tabela de formas de onda
-  AsyncDelay_c delayPlotWave(TIME_DELAY, ISMICROS); // time in micro second
+  AsyncDelay_c delayPlotWave(TIME_DELAY, ISMILI); // time in micro second
   for (;;)
   {
     if (delayPlotWave.isExpired())
     {
       delayPlotWave.repeat();
       const double aux = 1.0 + sin(2.0 * PI * CILCE_FREQ * timeWave);
-      ledcWrite(0, uint16_t(511.0*aux));
-      dacWrite(def_pin_DAC1, uint8_t(127.0*aux));
-      if (++timeWave >= CILCE_PERIODO) timeWave = 0;
-    }
-  }
-}
-
-static uint32_t time_delay_analog_ms = 1;
-void readWave(void *)
-{
-  uint32_t previousTimeAnalogMS = 0;  
-  for (;;)
-  {
-    uint32_t currentMilis = millis();
-    if ((currentMilis - previousTimeAnalogMS) >= time_delay_analog_ms)
-    {
-      previousTimeAnalogMS = currentMilis;
-      analogRead(def_pin_DAC1);
+      ledcWrite(0, uint16_t(511.0 * aux));
+      dacWrite(def_pin_DAC1, uint8_t(127.0 * aux));
+      if (++timeWave >= CILCE_PERIODO)
+        timeWave = 0;
     }
   }
 }
@@ -43,12 +32,16 @@ void setup()
   IIKit.setup();
   ledcAttachPin(def_pin_PWM, 0);
   ledcSetup(0, 1000, 10);
-  xTaskCreate(plotWave, "Task Wave", 5000, NULL, 1, NULL);
-  xTaskCreate(readWave, "Task Wave", 5000, NULL, 1, NULL);  
+  xTaskCreate(plotWave, "Task Wave", 4096, NULL, 1, NULL);
 }
 
+AsyncDelay_c delayReadtWave(500, ISMILI); // time in micro second
 void loop()
 {
   IIKit.loop();
-  time_delay_analog_ms = map(analogRead(def_pin_POT1), 0, 4095, 0, 120);
+  if (delayReadtWave.isExpired())
+  {
+    delayReadtWave.repeat();
+    time_delay_analog_ms = map(analogRead(def_pin_POT1), 0, 4095, 0, 120);
+  }
 }
